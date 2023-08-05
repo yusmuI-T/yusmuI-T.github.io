aa29c680a6a366ed663d0a4ff4e60f934cce1146
yusmui-t <yusmuinnovationsolutions@gmail.com>
2023-08-05 14:43:46 +0100

    making responsive

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const menu = document.getElementById('menu');
 const nav = document.getElementById('nav');
 const close = document.getElementById('close');
+const myform = document.getElementById('myform');
 
 
 menu.addEventListener('click', () =>{
@@ -13,4 +14,24 @@ close.addEventListener('click', () =>{
     menu.style.display = 'inline-block';
     nav.style.display = 'none';
     close.style.display = 'none';
+});
+
+// function to send user Input to P.R.O in whatsApp
+
+myform.addEventListener('submit',(event)=>{
+    event.preventDefault();
+   
+   const name = document.getElementById("name").value;
+   const email = document.getElementById("email").value;
+   const subject = document.getElementById("subject").value;
+   const message = document.getElementById("message").value;
+
+    const PhoneNumber = '2347045568176';
+    const encodedMessage = encodeURIComponent(`Name: ${name} 
+    \n Email: ${email}\n
+     Subject: ${subject} \n Message: ${message}`);
+
+     const whatsAppLink = "https://wa.me" + PhoneNumber + "?text=" + encodedMessage;
+
+     
 });
