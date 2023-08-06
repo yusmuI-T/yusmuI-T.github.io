901cf8d07888165e8dae02f28f853e335633088e
yusmui-t <yusmuinnovationsolutions@gmail.com>
2023-08-06 11:55:51 +0100

    changing phone number second time

--- main.js
+++ main.js
@@ -18,22 +18,3 @@ close.addEventListener('click', () =>{
 
 // function to send user Input to P.R.O in whatsApp
 
-myform.addEventListener('submit',(event)=>{
-    event.preventDefault();
-   
-   const name = document.getElementById("name").value;
-   const email = document.getElementById("email").value;
-   const subject = document.getElementById("subject").value;
-   const message = document.getElementById("message").value;
-
-    const PhoneNumber = '2347045568176';
-    const encodedMessage = encodeURIComponent(`Name: ${name} 
-    \n Email: ${email}\n
-     Subject: ${subject} \n Message: ${message}`);
-
-     const whatsAppLink = "https://wa.me" + PhoneNumber + "?text=" + encodedMessage;
-     window.open(whatsAppLink);
-
-     
-     
-});
